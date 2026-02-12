c59feae04c5434301c8359094f7ccb2d7d4301a5
Gebri Zsolt Pál <pgebrizsolt@gmail.com>
2026-02-12 09:33:52 +0100

    Update recipes.js

--- js/recipes.js
+++ js/recipes.js
@@ -1,1 +1,23 @@
+export const recipes = [
+    {
+        name: "Espresso",
+        ingredients: ["coffee"]
+    },
+    {
+        name: "Latte",
+        ingredients: ["coffee", "milk"]
+    },
+    {
+        name: "Cappuccino",
+        ingredients: ["coffee", "milk", "foam"]
+    },
+    {
+        name: "Americano",
+        ingredients: ["coffee", "water"]
+    }
+];
+
+export function getRandomRecipe() {
+    return recipes[Math.floor(Math.random() * recipes.length)];
+}
 
