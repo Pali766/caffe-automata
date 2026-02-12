00bb6a394f3b4a200960df79b80ddf0f63345c5e
Gebri Zsolt Pál <pgebrizsolt@gmail.com>
2026-02-12 09:34:16 +0100

    Update drag.js

--- js/drag.js
+++ js/drag.js
@@ -1,1 +1,20 @@
+import { setState, GameState } from "./gameState.js";
+import { getRandomRecipe } from "./recipes.js";
+import { currentOrder } from "./gameState.js";
+
+export function checkCoinInsert(coin, machine, uiElement) {
+    if (coin.position.distanceTo(machine.position) < 1) {
+
+        setState(GameState.ORDER_GENERATED);
+
+        const order = getRandomRecipe();
+        window.currentOrder = order;
+
+        uiElement.innerText = "Rendelés: " + order.name;
+
+        setTimeout(() => {
+            setState(GameState.MAKING_COFFEE);
+        }, 1000);
+    }
+}
 
