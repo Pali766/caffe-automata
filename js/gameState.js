c6e8a68a23e7fa0ba7a08e5c9b30e3686f901333
Gebri Zsolt Pál <pgebrizsolt@gmail.com>
2026-02-12 09:33:41 +0100

    Update gameState.js

--- js/gameState.js
+++ js/gameState.js
@@ -1,1 +1,21 @@
+export const GameState = {
+    WAITING_FOR_COIN: "WAITING_FOR_COIN",
+    ORDER_GENERATED: "ORDER_GENERATED",
+    MAKING_COFFEE: "MAKING_COFFEE",
+    CHECKING: "CHECKING",
+    SUCCESS: "SUCCESS",
+    FAIL: "FAIL"
+};
+
+export let currentState = GameState.WAITING_FOR_COIN;
+export let currentOrder = null;
+export let playerIngredients = [];
+
+export function setState(newState) {
+    currentState = newState;
+}
+
+export function resetPlayerIngredients() {
+    playerIngredients = [];
+}
 
